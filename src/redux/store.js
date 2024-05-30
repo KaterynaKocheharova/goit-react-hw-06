73a504d7868813138da36e1cc9d3a2a2d222b876
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-30 15:45:10 +0300

    add three versions

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,3 +1,32 @@
+// !!!!!!!!!!!!1 VERSION 1
+
+// import { configureStore, combineReducers } from "@reduxjs/toolkit";
+// import { persistStore, persistReducer } from "redux-persist";
+// import storage from "redux-persist/lib/storage";
+// import { filtersReducer } from "./filtersSlice";
+// import { contactsReducer } from "./contactsSlice";
+
+// const persistConfig = {
+//   key: "root",
+//   storage,
+//   whitelist: ["contacts"],
+// };
+
+// const rootReducer = combineReducers({
+//   filters: filtersReducer,
+//   contacts: contactsReducer,
+// });
+
+// const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// export const store = configureStore({
+//   reducer: persistedReducer,
+// });
+
+// export const persistor = persistStore(store);
+
+// !!! VERSION 2
+
 // import { configureStore } from "@reduxjs/toolkit";
 // import storage from "redux-persist/lib/storage";
 // import { persistStore, persistReducer } from "redux-persist";
@@ -19,6 +48,8 @@
 // });
 
 // export const persistor = persistStore(store);
+
+// !!!!!!!!!!!!1 VERSION 3
 
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
