8335e24238b796e69ef7dfa4a8f2f1a62ffcf890
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-30 15:53:40 +0300

    done

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,83 +1,35 @@
-// !!!!!!!!!!!!1 VERSION 1
-
-// import { configureStore, combineReducers } from "@reduxjs/toolkit";
-// import { persistStore, persistReducer } from "redux-persist";
-// import storage from "redux-persist/lib/storage";
-// import { filtersReducer } from "./filtersSlice";
-// import { contactsReducer } from "./contactsSlice";
-
-// const persistConfig = {
-//   key: "root",
-//   storage,
-//   whitelist: ["contacts"],
-// };
-
-// const rootReducer = combineReducers({
-//   filters: filtersReducer,
-//   contacts: contactsReducer,
-// });
-
-// const persistedReducer = persistReducer(persistConfig, rootReducer);
-
-// export const store = configureStore({
-//   reducer: persistedReducer,
-// });
-
-// export const persistor = persistStore(store);
-
-// !!! VERSION 2
-
-// import { configureStore } from "@reduxjs/toolkit";
-// import storage from "redux-persist/lib/storage";
-// import { persistStore, persistReducer } from "redux-persist";
-// import { filtersReducer } from "./filtersSlice";
-// import { contactsReducer } from "./contactsSlice";
-
-// const persistConfig = {
-//   key: "contacts",
-//   storage,
-// };
-
-// const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
-// export const store = configureStore({
-//   reducer: {
-//     filters: filtersReducer,
-//     contacts: persistedReducer,
-//   },
-// });
-
-// export const persistor = persistStore(store);
-
-// !!!!!!!!!!!!1 VERSION 3
-
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
-import { persistStore, persistReducer } from "redux-persist";
-import { persistCombineReducers } from "redux-persist";
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 import { filtersReducer } from "./filtersSlice";
 import { contactsReducer } from "./contactsSlice";
-import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 const persistConfig = {
-  key: "root",
+  key: "contacts",
   storage,
-  stateReconciler: autoMergeLevel2,
-  blacklist: ["filters"],
 };
 
-const rootReducer = persistCombineReducers(persistConfig, {
-  filters: filtersReducer,
-  contacts: contactsReducer,
-});
-
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
-  reducer: persistedReducer,
+  reducer: {
+    filters: filtersReducer,
+    contacts: persistedReducer,
+  },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
-      serializableCheck: false,
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
     }),
 });
 
