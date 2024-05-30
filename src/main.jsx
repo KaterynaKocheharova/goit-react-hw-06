d6088aa0a3b321b171f9e74e310468e9f8543ac8
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-30 15:12:25 +0300

    do save to local storage

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { store } from "./redux/store.js";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistor } from "./redux/store.js";
 import "modern-normalize";
 import App from "./App.jsx";
 import "./index.css";
@@ -9,7 +11,9 @@ import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor}>
+        <App />
+      </PersistGate>
     </Provider>
   </React.StrictMode>
 );
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,21 @@
 import { configureStore } from "@reduxjs/toolkit";
+import storage from "redux-persist/lib/storage";
+import { persistStore, persistReducer } from "redux-persist";
 import { filtersReducer } from "./filtersSlice";
 import { contactsReducer } from "./contactsSlice";
+
+const persistConfig = {
+  key: "contacts",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, contactsReducer);
 
 export const store = configureStore({
   reducer: {
     filters: filtersReducer,
-    contacts: contactsReducer,
+    contacts: persistedReducer,
   },
 });
+
+export const persistor = persistStore(store);
