c841eab65eff10b3adec4f800f1dbc6567e037b6
Kateryna Kocheharova <kot41901@gmail.com>
2024-05-30 15:25:29 +0300

    fix not sterializable error

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,21 +1,53 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import storage from "redux-persist/lib/storage";
+// import { persistStore, persistReducer } from "redux-persist";
+// import { filtersReducer } from "./filtersSlice";
+// import { contactsReducer } from "./contactsSlice";
+
+// const persistConfig = {
+//   key: "contacts",
+//   storage,
+// };
+
+// const persistedReducer = persistReducer(persistConfig, contactsReducer);
+
+// export const store = configureStore({
+//   reducer: {
+//     filters: filtersReducer,
+//     contacts: persistedReducer,
+//   },
+// });
+
+// export const persistor = persistStore(store);
+
 import { configureStore } from "@reduxjs/toolkit";
 import storage from "redux-persist/lib/storage";
 import { persistStore, persistReducer } from "redux-persist";
+import { persistCombineReducers } from "redux-persist";
 import { filtersReducer } from "./filtersSlice";
 import { contactsReducer } from "./contactsSlice";
+import autoMergeLevel2 from "redux-persist/lib/stateReconciler/autoMergeLevel2";
 
 const persistConfig = {
-  key: "contacts",
+  key: "root",
   storage,
+  stateReconciler: autoMergeLevel2,
+  blacklist: ["filters"],
 };
 
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
+const rootReducer = persistCombineReducers(persistConfig, {
+  filters: filtersReducer,
+  contacts: contactsReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 export const store = configureStore({
-  reducer: {
-    filters: filtersReducer,
-    contacts: persistedReducer,
-  },
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
 });
 
 export const persistor = persistStore(store);
